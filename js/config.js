197a57a50a4fc6e99e3c951d58a77f380d37cbc9
sycicu <88899141+honest4802@users.noreply.github.com>
2025-07-09 12:14:37 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -121,43 +121,43 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: true
+        adult: false
     },
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
+        adult: false
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
+        adult: false
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
+        adult: false
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
+        adult: false
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
+        adult: false
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
+        adult: false,
         detail: 'https://hsckzy.vip'
     },
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
+        adult: false
     },
 
     // 下面是资源失效率高的API源，不建议使用
